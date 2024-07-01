f97cbf580fd18004e2975a0798583e9c95e46e35
ITN\itnam <itnams12@gmail.com>
2024-07-01 19:38:38 +0700

    feat(header): add header and bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Route } from '@angular/router';
+import { HomeComponent } from '@shop/home';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+];
